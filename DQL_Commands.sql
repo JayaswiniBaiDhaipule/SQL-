3f0baea48d8d9225d2f57769822d8e87a79e4851
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2026-08-12 07:12:51 +0530

    Data Query Language

---
+++ DQL_Commands.sql
@@ -0,0 +1,235 @@
+USE MyDatabase;
+
+-- This is a comment
+
+/*
+This is a comment 
+*/
+
+/* ==============================================================================
+   SELECT ALL COLUMNS
+=============================================================================== */
+
+-- Retrieve All Customer Data
+
+SELECT * 
+FROM MyDatabase.dbo.customers;
+
+-- Retrieve All Order Data
+SELECT *
+FROM MyDatabase.dbo.orders;
+
+/* ==============================================================================
+   SELECT FEW COLUMNS
+=============================================================================== */
+
+
+-- Retrieve each customer's name, country, and score.
+SELECT
+	first_name,
+	country,
+	score
+FROM MyDatabase.dbo.customers;
+
+/* ==============================================================================
+   WHERE
+=============================================================================== */
+
+-- Retrieve customers with a score not equal to 0
+SELECT * 
+FROM MyDatabase.dbo.customers
+WHERE score <> 0;
+
+SELECT * 
+FROM MyDatabase.dbo.customers
+WHERE score != 0;
+
+-- Retrieve customers from Germany
+
+SELECT *
+FROM MyDatabase.dbo.customers
+WHERE country = 'Germany';
+
+-- Retrieve the name and country of customers from Germany
+SELECT 
+	first_name,
+	country
+FROM MyDatabase.dbo.customers
+WHERE country = 'Germany';
+
+/* ==============================================================================
+   ORDER BY
+=============================================================================== */
+
+/* Retrieve all customers and 
+   sort the results by the highest score first. */
+
+   SELECT *
+   FROM MyDatabase.dbo.customers
+   ORDER BY score DESC;
+
+   
+/* Retrieve all customers and 
+   sort the results by the lowest score first. */
+
+   SELECT *
+   FROM MyDatabase.dbo.customers
+   ORDER BY score ASC;
+
+   /* Retrieve all customers and 
+   sort the results by the country. */
+
+   SELECT *
+   FROM MyDatabase.dbo.customers
+   ORDER BY country ASC,
+		   score DESC;
+
+/* Retrieve the name, country, and score of customers 
+   whose score is not equal to 0
+   and sort the results by the highest score first. */
+SELECT 
+	first_name,
+	country,
+	score
+FROM MyDatabase.dbo.customers
+WHERE score <> 0
+ORDER BY score DESC;
+
+
+/* ==============================================================================
+   GROUP BY
+=============================================================================== */
+
+-- Find the total score for each country
+
+SELECT 
+	country,
+	SUM(score) as Total_Score
+FROM MyDatabase.dbo.customers
+GROUP BY country;
+
+/* This will not work because 'first_name' is neither part of the GROUP BY 
+   nor wrapped in an aggregate function. SQL doesn't know how to handle this column. */
+
+ SELECT 
+    country,
+    first_name,
+    SUM(score) AS total_score
+FROM customers
+GROUP BY country;
+
+
+-- Find the total score and total number of customers for each country
+
+SELECT
+	country,
+	SUM(score) AS Total_Score,
+	COUNT(Country) AS Customers_Count
+FROM MyDatabase.dbo.customers
+GROUP BY country;
+
+
+/* ==============================================================================
+   HAVING
+=============================================================================== */
+
+/* Find the average score for each country
+   and return only those countries with an average score greater than 430 */
+SELECT 
+country,
+AVG(score) AS Average_Score
+FROM MyDatabase.dbo.customers
+GROUP BY country
+HAVING AVG(score) > 430;
+
+/* Find the average score for each country
+   considering only customers with a score not equal to 0
+   and return only those countries with an average score greater than 430 */
+SELECT
+	country,
+	AVG(score) AS avg_score
+FROM MyDatabase.dbo.customers
+WHERE score <> 0
+GROUP BY country
+HAVING AVG(score) > 40;
+
+/* ==============================================================================
+   DISTINCT
+=============================================================================== */
+
+-- Return Unique list of all countries
+SELECT 
+	DISTINCT country
+FROM MyDatabase.dbo.customers;
+
+/* ==============================================================================
+   TOP
+=============================================================================== */
+
+-- Retrieve only 3 Customers
+SELECT
+	TOP 3 *
+FROM MyDatabase.dbo.customers;
+
+-- Retrieve the Top 3 Customers with the Highest Scores
+SELECT 
+	TOP 3 * 
+FROM MyDatabase.dbo.customers
+ORDER BY score DESC;
+
+-- Retrieve the Lowest 2 Customers based on the score
+
+SELECT 
+	TOP 2 *
+FROM MyDatabase.dbo.customers
+ORDER BY score ASC;
+
+
+-- Get the Two Most Recent Orders
+
+SELECT * FROM orders;
+
+SELECT
+TOP 2 *
+FROM MyDatabase.dbo.orders
+ORDER BY order_date DESC;
+
+
+/* ==============================================================================
+   All Together
+=============================================================================== */
+
+/* Calculate the average score for each country 
+   considering only customers with a score not equal to 0
+   and return only those countries with an average score greater than 430
+   and sort the results by the highest average score first. */
+
+   SELECT
+	   country,
+	   AVG(score) AS avg_score
+   FROM MyDatabase.dbo.customers
+   WHERE score <> 0
+   GROUP BY country
+   HAVING AVG(score) > 430
+   ORDER BY AVG(score) DESC;
+
+   /* ============================================================================== 
+   COOL STUFF - Additional SQL Features
+=============================================================================== */
+
+-- Execute multiple queries at once
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+/* Selecting Static Data */
+-- Select a static or constant value without accessing any table
+SELECT 123 AS static_number;
+
+SELECT 'Hello' AS static_string;
+
+-- Assign a constant value to a column in a query
+SELECT
+    id,
+    first_name,
+    'New Customer' AS customer_type
+FROM customers;
